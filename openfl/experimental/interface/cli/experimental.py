22b9d6b770c02c06392228fe910c3ce6c2a89289
Mansi Sharma <mansi.sharma@intel.com>
2023-12-11 11:37:20 -0800

    addition of fx experimental activate
    Signed-off-by: Mansi Sharma <mansi.sharma@intel.com>

--- openfl/experimental/interface/cli/experimental.py
+++ openfl/experimental/interface/cli/experimental.py
@@ -15,6 +15,13 @@ def experimental(context):
     """Manage Experimental Environment."""
     context.obj["group"] = "experimental"
 
+@experimental.command(name="activate")
+def activate():
+    """Activate experimental environment."""
+    settings = Path("~").expanduser().joinpath(
+        ".openfl").resolve()
+
+    os.path.join(settings, "experimental")
 
 @experimental.command(name="deactivate")
 def deactivate():
